59a7ecc50560a0a1cbfdca0f87337ab048dc4ef4
raorao <savinirs@gmail.com>
2015-02-25 20:47:44 -0800

    adds check function that handles strings
    - removes dummy test

--- diff.js
+++ diff.js
@@ -1,11 +1,17 @@
+function check(oldVal, newVal) {
+  return oldVal === newVal
+}
+
+
+
 assert = function(message,assertion) {
   if ( !assertion() ) {
    throw( new Error("test failed: " + message) )
   }
 }
 
-assert('one should equal one', function() {
-  return 1 == 1
+assert('check handles strings', function() {
+  return check('str', 'str')
 })
 
 console.log('all tests passed')
