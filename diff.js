91bc4142e2dceea3d06bb0be38c21455ae67e90c
raorao <savinirs@gmail.com>
2015-02-25 20:59:33 -0800

    check handles primitive values of same type
    no edge case protection

--- diff.js
+++ diff.js
@@ -1,4 +1,12 @@
 function check(oldVal, newVal) {
+  if(typeof(oldVal) === 'number' && isNaN(oldVal)) {
+    return typeof(newVal) === 'number' && isNaN(newVal);
+  };
+
+  if(typeof(oldVal) === 'number' && !isFinite(oldVal)) {
+    return typeof(newVal) === 'number' && !isFinite(newVal)
+  }
+
   return oldVal === newVal
 }
 
@@ -13,5 +21,59 @@ assert = function(message,assertion) {
 assert('check handles strings', function() {
   return check('str', 'str')
 })
+
+assert('check handles strings', function() {
+  return check('str', 'str1') === false
+})
+
+assert('check handles integers', function() {
+  return check(1, 1)
+})
+
+assert('check handles integers', function() {
+  return check(1, 2) === false
+})
+
+assert('check handles boolean values', function() {
+  return check(true, true)
+})
+
+assert('check handles boolean values', function() {
+  return check(true, false) === false
+})
+
+assert('check handles floats', function() {
+  return check(1.1, 1.1)
+})
+
+assert('check handles floats', function() {
+  return check(1.1, 1.2) === false
+})
+
+assert('check handles two null values', function() {
+  return check(null,null)
+})
+
+assert('check handles two undefined values', function() {
+  return check(undefined, undefined)
+})
+
+assert('check handles two NaN values', function() {
+  return check(NaN, NaN)
+})
+
+assert('check handles two Infinity values', function() {
+  return check(Infinity, Infinity)
+})
+
+assert('check handles two -Infinity values', function() {
+  return check(-Infinity, -Infinity)
+})
+
+
+
+
+
+
 
 console.log('all tests passed')
